f813d7e68f5c623e7078da050e414760bcaf1788
Rakesh Gowda <rakesh96.gowda28@gmail.com>
2018-02-24 12:54:28 +0530

    Project Setup Completed Successfully!!!

---
+++ awbackend/src/main/java/com/rakz/awbackend/App.java
@@ -0,0 +1,13 @@
+package com.rakz.awbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
